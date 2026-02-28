f9ea24472d4ec227089154ce52302998d2828bcd
Meshrr <volframhome@gmail.com>
2026-03-01 00:15:53 +0500

    add func to create_tables on startup

--- parser/main.py
+++ parser/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 
+
+from parser.database import create_tables
+
 app = FastAPI(
     title="Wildberries Parser API",
     description="Parser of products from Wildberries"
 )
+
+@app.on_event()
+async def on_startup():
+    await create_tables()
+
 
 @app.get("/")
 async def root():
