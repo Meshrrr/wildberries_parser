d8847df6d5184acbb0451ae2718de1e5e634fc62
Meshrr <volframhome@gmail.com>
2026-03-01 00:15:24 +0500

    update database settings

--- parser/database.py
+++ parser/database.py
@@ -1,11 +1,25 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from contextlib import asynccontextmanager
+
 
 DATABASE_URL = "sqlite+aiosqlite:///wb_parser.sqlite3"
+
 engine = create_async_engine(DATABASE_URL,
                              echo=False)
 
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
+class Base(DeclarativeBase):
+    pass
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_db() -> AsyncSession:
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
